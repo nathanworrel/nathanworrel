381c68e2b4c2361a9d555454b11e6a5aa8057082
nworrel <nworrel@MGC1P0643W0F.fbpld77.ford.com>
2022-07-08 15:23:04 -0500

    initalized server and client

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-res.json({ message: "Hello from server!" });
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
