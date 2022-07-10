47c406c22ef673efc2a26a33f1874cfee3775d11
nworrel <nworrel@MGC1P0643W0F.fbpld77.ford.com>
2022-07-10 13:01:42 -0500

    basic setup for Welcome and Work Experience Page

--- client/src/welcomePage/Welcome.tsx
+++ client/src/welcomePage/Welcome.tsx
@@ -5,8 +5,19 @@ function Welcome() {
     return(
         <main>
             <Header/>
-            <div>
-                <p>Welcome</p>
+            <h1 id="welcome">
+                Welcome!!
+            </h1>
+            <div id="welcome-information">
+                <div id={"welcome-image"}>
+                    <p>Put Image Here</p>
+                </div>
+                <div id="welcome-text">
+                    <p>
+                        Hello!! I'm Nathaniel Worrel a Senior at the University of Michigan studying computer science.
+                        I made this website to show help showcase who I am and what I am capiable of.
+                    </p>
+                </div>
             </div>
             <Footer/>
         </main>
--- client/src/workExperiencePage/WorkExperience.tsx
+++ client/src/workExperiencePage/WorkExperience.tsx
@@ -4,8 +4,17 @@ function WorkExperience() {
     return(
         <main>
             <Header/>
-            <div>
-                <p>Work Experience</p>
+            <h1>Work Experience</h1>
+            <div id="work-experience-info">
+                <div>
+                    <h3>Ford Software Designer</h3>
+                </div>
+                <div>
+                    <h3>MindHome Product Designer/Software Engineer</h3>
+                </div>
+                <div>
+                    
+                </div>
             </div>
         </main>
     )
